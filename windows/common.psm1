b2b9ad37efa1c341de3b069027443f4d5e010d8a
Hoang NGUYEN <baauco@gmail.com>
2022-03-17 23:26:45 +0700

    Refactor

--- windows/common.psm1
+++ windows/common.psm1
@@ -1,139 +1,23 @@
-# Get-PSDrive
-# Get-ChildItem -Path Env:
-
 function Assert-Elevated {
   # Get the ID and security principal of the current user account
   $MyIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-  $MyPrincipal = new-object System.Security.Principal.WindowsPrincipal($MyIdentity)
+  $MyPrincipal = New-Object System.Security.Principal.WindowsPrincipal($MyIdentity)
   # Check to see if we are currently running "as Administrator"
   return $MyPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
-function New-RelativeSymbolicItem {
-  <#
-  .PARAMETER Path
-  Location of the symbolic link
-  .PARAMETER Target
-  Location of the real file
-  .EXAMPLE
-  New-RelativeSymbolicItem -Path "~\.wslconfig" -Target ".\configs\wslconfig"
-  #>
-  param (
-    [Parameter(Mandatory = $true)]
-    [string]$Path,
-    [Parameter(Mandatory = $true)]
-    [string]$Target
-  )
-
-  New-Item -ItemType SymbolicLink -Path $Path -Target $(Resolve-Path -LiteralPath $Target) -Force -ErrorAction SilentlyContinue
+function Update-Upgrade {
+  Update-Help -Force
+  Update-Module -Force
 }
 
-function Update-EveryModule {
-  <#
-  .SYNOPSIS
-  Updates all modules from the PowerShell gallery.
-  .DESCRIPTION
-  Updates all local modules that originated from the PowerShell gallery.
-  Removes all old versions of the modules.
-  .PARAMETER ExcludedModules
-  Array of modules to exclude from updating.
-  .PARAMETER SkipMajorVersion
-  Skip major version updates to account for breaking changes.
-  .PARAMETER KeepOldModuleVersions
-  Array of modules to keep the old versions of.
-  .PARAMETER ExcludedModulesForRemoval
-  Array of modules to exclude from removing old versions of.
-  The Az module is excluded by default.
-  .EXAMPLE
-  Update-EveryModule -ExcludedModulesForRemoval 'Az'
-  .NOTES
-  Created by Barbara Forbes
-  @ba4bes
-  .LINK
-  https://4bes.nl
-  #>
-  [CmdletBinding(SupportsShouldProcess = $true)]
-  param (
-    [parameter()]
-    [array]$ExcludedModules = @(),
-    [parameter()]
-    [switch]$SkipMajorVersion,
-    [parameter()]
-    [switch]$KeepOldModuleVersions,
-    [parameter()]
-    [array]$ExcludedModulesForRemoval = @("Az")
-  )
-  # Get all installed modules that have a newer version available
-  Write-Verbose "Checking all installed modules for available updates."
-  $CurrentModules = Get-InstalledModule | Where-Object { $ExcludedModules -notcontains $_.Name -and $_.repository -eq "PSGallery" }
-
-  # Walk through the Installed modules and check if there is a newer version
-  $CurrentModules | ForEach-Object {
-    Write-Verbose "Checking $($_.Name)"
-    Try {
-      $GalleryModule = Find-Module -Name $_.Name -Repository PSGallery -ErrorAction Stop
-    }
-    Catch {
-      Write-Error "Module $($_.Name) not found in gallery $_"
-      $GalleryModule = $null
-    }
-    if ($GalleryModule.Version -gt $_.Version) {
-      if ($SkipMajorVersion -and $GalleryModule.Version.Split('.')[0] -gt $_.Version.Split('.')[0]) {
-        Write-Warning "Skipping major version update for module $($_.Name). GalleryVersion: $($GalleryModule.Version), local version $($_.Version)"
-      }
-      else {
-        Write-Verbose "$($_.Name) will be updated. GalleryVersion: $($GalleryModule.Version), local version $($_.Version)"
-        try {
-          if ($PSCmdlet.ShouldProcess(
-                      ("Module {0} will be updated to version {1}" -f $_.Name, $GalleryModule.Version),
-              $_.Name,
-              "Update-Module"
-            )
-          ) {
-            Update-Module $_.Name -ErrorAction Stop -Force
-            Write-Verbose "$($_.Name)  has been updated"
-          }
-        }
-        Catch {
-          Write-Error "$($_.Name) failed: $_ "
-          continue
-
-        }
-        if ($KeepOldModuleVersions -ne $true) {
-          Write-Verbose "Removing old module $($_.Name)"
-          if ($ExcludedModulesForRemoval -contains $_.Name) {
-            Write-Verbose "$($allversions.count) versions of this module found [ $($module.name) ]"
-            Write-Verbose "Please check this manually as removing the module can cause instability."
-          }
-          else {
-            try {
-              if ($PSCmdlet.ShouldProcess(
-                              ("Old versions will be uninstalled for module {0}" -f $_.Name),
-                  $_.Name,
-                  "Uninstall-Module"
-                )
-              ) {
-                Get-InstalledModule -Name $_.Name -AllVersions
-                | Where-Object { $_.version -ne $GalleryModule.Version }
-                | Uninstall-Module -Force -ErrorAction Stop
-                Write-Verbose "Old versions of $($_.Name) have been removed"
-              }
-            }
-            catch {
-              Write-Error "Uninstalling old module $($_.Name) failed: $_"
-            }
-          }
-        }
-      }
-    }
-    elseif ($null -ne $GalleryModule) {
-      Write-Verbose "$($_.Name) is up to date"
-    }
+function Install-OMP {
+  @(
+    'oh-my-posh',
+    'posh-git'
+  ) | ForEach-Object {
+    Install-Module -Name $_ -Scope CurrentUser -Force
   }
-}
-
-function Install-dotNet() {
-  pwsh -NoProfile -ExecutionPolicy Unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Channel Current"
 }
 
 function Install-OpenSSH {
@@ -145,18 +29,42 @@ function Install-OpenSSH {
   Get-Service sshd | Set-Service -StartupType Automatic -PassThru | Start-Service
 
   # Confirm the Firewall rule is configured. It should be created automatically by setup
-  if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+  if (!(Get-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
     New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
   }
 }
 
-function Install-WSL() {
+function Install-WSL {
   # Get-WindowsOptionalFeature -Online
   Enable-WindowsOptionalFeature -Online -All -NoRestart -FeatureName `
-    @("VirtualMachinePlatform", "Microsoft-Windows-Subsystem-Linux") | Out-Null
+  @('VirtualMachinePlatform', 'Microsoft-Windows-Subsystem-Linux') | Out-Null
+}
+
+function Install-dotNet {
+  pwsh -NoProfile -ExecutionPolicy Unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Channel Current"
 }
 
 function Set-ComputerName {
-  $ComputerName = Read-Host -Prompt "Enter New Computer Name"
+  $ComputerName = Read-Host -Prompt 'Enter New Computer Name'
   Rename-Computer -NewName $ComputerName
+}
+
+function shift {
+  # Prevent user from running this script
+  exit 1
+
+  $args = $args | Select-Object -Skip 1
+}
+
+function Get-EnvVariable {
+  # Prevent user from running this script
+  exit 1
+
+  # Env, aliases, functions and more are stored in virtual drives
+  # Use this command to get them
+  Get-PSDrive
+
+  # Get all environment variables
+  # or use `dir` because `dir` is an alias
+  Get-ChildItem -Path Env:
 }
--- windows/install.ps1
+++ windows/install.ps1
@@ -1,76 +1,86 @@
 # Self elevate administrative permissions in this script
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-  Start-Process pwsh "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+if (!(Assert-Elevated)) {
+  Start-Process pwsh '-NoProfile -ExecutionPolicy Bypass -File "$PSCommandPath"' -Verb 'RunAs'
   exit
 }
 
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
-
-Import-Module -Name .\packages\common.psm1
-
-function Set-ComputerName {
-  $ComputerName = Read-Host -Prompt Enter New Computer Name
-  Rename-Computer -NewName $ComputerName
-}
+Import-Module -Name .\common.psm1
 
 function Uninstall-Bloat {
   # Get-AppxPackage | Format-Table -Property Name,Version,PackageFullName
   @(
-    "king.com.CandyCrushFriends",
-    "Microsoft.3DBuilder",
-    "Microsoft.Print3D",
-    "Microsoft.BingNews",
-    "Microsoft.OneConnect",
-    "Microsoft.Microsoft3DViewer",
-    "HolographicFirstRun",
-    "Microsoft.MixedReality.Portal"
-    "Microsoft.MicrosoftSolitaireCollection",
-    "Microsoft.Getstarted",
-    "Microsoft.WindowsFeedbackHub",
-    "Microsoft.XboxApp",
-    "Fitbit.FitbitCoach",
-    "4DF9E0F8.Netflix"
+    'king.com.CandyCrushFriends',
+    'Microsoft.3DBuilder',
+    'Microsoft.Print3D',
+    'Microsoft.BingNews',
+    'Microsoft.OneConnect',
+    'Microsoft.Microsoft3DViewer',
+    'HolographicFirstRun',
+    'Microsoft.MixedReality.Portal'
+    'Microsoft.MicrosoftSolitaireCollection',
+    'Microsoft.Getstarted',
+    'Microsoft.WindowsFeedbackHub',
+    'Microsoft.XboxApp',
+    'Fitbit.FitbitCoach',
+    '4DF9E0F8.Netflix'
   ) | ForEach-Object {
     Get-AppxPackage -Name $_ | Remove-AppxPackage
   }
 }
 
-function Install-BasePackages {
+function Install-Packages {
   @(
-    "PowerShellGet",
-    "PSReadLine",
-    "posh-git"
+    'PowerShellGet',
+    'PSReadLine'
   ) | ForEach-Object {
-    Install-Module -Name $_ -Force
+    Install-Module -Name $_ -Scope CurrentUser -Force
   }
+
+  Install-OpenSSH
+  Install-OMP
+  Install-WSL
 }
 
 function Install-Configs {
-  New-RelativeSymbolicItem -Path ~\.ssh\config -Target .\configs\ssh_config
-  New-RelativeSymbolicItem -Path ~\.wslconfig -Target .\configs\wslconfig
-
   $ProfileDir = Split-Path -Parent $Profile
-  New-Item -Path $ProfileDir -ItemType Directory -Force -ErrorAction SilentlyContinue
+  $PluginsDir = Join-Path $ProfileDir 'plugins'
+  New-Item -Path $ProfileDir -ItemType Directory -ErrorAction SilentlyContinue -Force
+  New-Item -Path $PluginsDir -ItemType Directory -ErrorAction SilentlyContinue -Force
+
+  New-Item -ItemType SymbolicLink -Path "$HOME\.ssh\config" `
+    -Target $(Resolve-Path -LiteralPath .\configs\ssh_config) -Force
+  New-Item -ItemType SymbolicLink -Path "$HOME\.wslconfig" `
+    -Target $(Resolve-Path -LiteralPath .\configs\wslconfig) -Force
 
   # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles
-  Get-ChildItem -Path .\runcoms |
-  ForEach-Object {
-    New-Item -ItemType SymbolicLink -Path "${ProfileDir}\$($_.Name)" -Target $_.FullName -Force
-  }
+  Get-ChildItem -Path '.\runcoms\*' -Include '*.ps1' |
+    ForEach-Object {
+      New-Item -ItemType SymbolicLink -Path "$ProfileDir\$($_.Name)" `
+        -Target $_.FullName -Force
+    }
+  Get-ChildItem -Path '.\runcoms\plugins\' |
+    ForEach-Object {
+      New-Item -ItemType SymbolicLink -Path "$PluginsDir\$($_.Name)" `
+        -Target $_.FullName -Force
+    }
 
-  Get-ChildItem -Path .\configs\git\ |
-  ForEach-Object {
-    New-Item -ItemType SymbolicLink -Path "~\.$($_.Name)" -Target $_.FullName -Force
-  }
+  Get-ChildItem -Path '.\configs\git\' |
+    ForEach-Object {
+      New-Item -ItemType SymbolicLink -Path "$HOME\.$($_.Name)" `
+        -Target $_.FullName -Force
+    }
+  Add-Content "$HOME\.gitconfig.local" $null
 
-  Get-ChildItem -Path .\configs\gnupg\ |
-  ForEach-Object {
-    New-Item -ItemType SymbolicLink -Path "~\.gnupg\$($_.Name)" -Target $_.FullName -Force
-  }
+  Get-ChildItem -Path '.\configs\gnupg\' |
+    ForEach-Object {
+      New-Item -ItemType SymbolicLink -Path "$HOME\.gnupg\$($_.Name)" -Target $_.FullName -Force
+    }
 }
 
 function main {
-  Install-BasePackages
+  Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+  Update-Upgrade
+  Install-Packages
   Install-Configs
 }
 
