7961d1f51a73f488db23e6068fc00a6af35d18b7
Hoang NGUYEN <baauco@gmail.com>
2022-03-18 09:43:16 +0700

    nix(sshd): Link sshd_config

--- nix/install.sh
+++ nix/install.sh
@@ -50,6 +50,7 @@ function link_config {
 
   # ln -rs "$@" ./configs/ssh_config "$XDG_CONFIG_HOME"/ssh/config
   ln -rs "$@" ./configs/ssh_config "$HOME"/.ssh/config
+  ln -rs "$@" ./configs/sshd_config /etc/ssh/sshd_config
   ln -rs "$@" ./configs/git/gitconfig "$XDG_CONFIG_HOME"/git/config
   ln -rs "$@" ./configs/git/gitignore.global "$XDG_CONFIG_HOME"/git/ignore.global
   touch "$XDG_CONFIG_HOME"/git/config.local
