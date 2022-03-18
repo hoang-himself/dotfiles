f414bb59f29f0ffa67d5aa2cd8ddb15fc7fec469
Hoang NGUYEN <baauco@gmail.com>
2022-03-19 00:05:58 +0700

    win(rc): Add completion

--- windows/runcoms/profile.ps1
+++ windows/runcoms/profile.ps1
@@ -1,6 +1,7 @@
 Import-Module oh-my-posh
 # oh-my-posh prompt init pwsh --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/v$(oh-my-posh --version)/themes/jandedobbeleer.omp.json" | Invoke-Expression
 oh-my-posh prompt init pwsh --config "$env:LOCALAPPDATA\oh-my-posh\themes\jandedobbeleer.omp.json" | Invoke-Expression
+oh-my-posh completion powershell | Out-String | Invoke-Expression
 
 Push-Location (Split-Path -Parent $Profile)
 @('plugins', 'functions', 'aliases', 'exports', 'extra') | Where-Object { Test-Path "$_.ps1" } | ForEach-Object -Process { Invoke-Expression ". .\$_.ps1" }
