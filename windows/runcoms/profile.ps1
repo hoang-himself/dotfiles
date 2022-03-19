489e6b9170ed47139f82fff9163b64449204e010
Hoang NGUYEN <baauco@gmail.com>
2022-03-19 16:40:32 +0700

    win(rc): Add keybinding to exit shell

--- windows/runcoms/profile.ps1
+++ windows/runcoms/profile.ps1
@@ -3,6 +3,9 @@ Import-Module oh-my-posh
 oh-my-posh prompt init pwsh --config "$env:LOCALAPPDATA\oh-my-posh\themes\jandedobbeleer.omp.json" | Invoke-Expression
 oh-my-posh completion powershell | Out-String | Invoke-Expression
 
+# https://docs.microsoft.com/en-us/powershell/module/psreadline/about/about_psreadline
+Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
+
 Push-Location (Split-Path -Parent $Profile)
 @('plugins', 'functions', 'aliases', 'exports', 'extra') | Where-Object { Test-Path "$_.ps1" } | ForEach-Object -Process { Invoke-Expression ". .\$_.ps1" }
 Pop-Location
