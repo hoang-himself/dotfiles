c0e66fa368f20a6e0eb69f3c880eadff52cb31db
Hoang NGUYEN <baauco@gmail.com>
2022-03-19 21:10:04 +0700

    devcont(conf): Refactor

--- devcont/install.sh
+++ devcont/install.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
 function link_dotfiles {
+  mkdir -p "$HOME"/.{gnupg,ssh}
   ln -rs "$@" ./configs/ssh/config ~/.ssh/config
 
   for rcfile in ./runcom/*; do
-    ln -rs "$@" "$rcfile" ~/."$(basename "$rcfile")"
+    ln -rs "$@" "$rcfile" "$HOME/.$(basename "$rcfile")"
   done
 
   for rcfile in ../common/git/*; do
-    ln -rs "$@" "$rcfile" ~/."$(basename "$rcfile")"
+    ln -rs "$@" "$rcfile" "$HOME/.$(basename "$rcfile")"
   done
 
   for rcfile in ../common/gnupg/*; do
-    ln -rs "$@" "$rcfile" ~/.gnupg/"$(basename "$rcfile")"
+    ln -rs "$@" "$rcfile" "$HOME/.gnupg/$(basename "$rcfile")"
   done
 }
 
