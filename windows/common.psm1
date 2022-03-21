901dff700a089d4d9d190b0b2394faf5db042fd2
Hoang NGUYEN <baauco@gmail.com>
2022-03-21 15:40:17 +0700

    🚚 win(conf): Move used bootstraps script to install script

--- windows/common.psm1
+++ windows/common.psm1
@@ -6,62 +6,6 @@ function Assert-Elevated {
   return $MyPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
-function Install-OMP {
-  $ProfileDir = Split-Path -Parent $Profile
-  $PluginsDir = Join-Path $ProfileDir 'plugins'
-
-  @(
-    'oh-my-posh',
-    'posh-git',
-    'Terminal-Icons'
-  ) | ForEach-Object {
-    Install-Module -Name $_ -Scope CurrentUser -Force
-  }
-
-  # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles
-  Get-ChildItem -Path '.\runcoms\*' -Include '*.ps1' |
-    ForEach-Object {
-      New-Item -ItemType SymbolicLink -Path "$ProfileDir\$($_.Name)" `
-        -Target $_.FullName -Force
-    }
-  Get-ChildItem -Path '.\runcoms\plugins\' |
-    ForEach-Object {
-      New-Item -ItemType SymbolicLink -Path "$PluginsDir\$($_.Name)" `
-        -Target $_.FullName -Force
-    }
-}
-
-function Install-OpenSSH {
-  # https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse
-  Add-WindowsCapability -Online -Name OpenSSH.Client
-  Get-Service ssh-agent | Set-Service -StartupType Automatic -PassThru | Start-Service
-
-  Add-WindowsCapability -Online -Name OpenSSH.Server
-  Get-Service sshd | Set-Service -StartupType Automatic -PassThru | Start-Service
-
-  # Confirm the Firewall rule is configured. It should be created automatically by setup
-  if ( -not (Get-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
-    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-  }
-
-  New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.ssh\config" `
-    -Target $(Resolve-Path -LiteralPath .\configs\ssh_config) -Force
-  # icacls.exe "C:\ProgramData\ssh\administrators_authorized_keys" /inheritance:r /grant "Administrators:F" /grant "SYSTEM:F"
-  New-Item -ItemType SymbolicLink -Path "$env:ProgramData\ssh\sshd_config" `
-    -Target $(Resolve-Path -LiteralPath .\configs\sshd_config) -Force
-  New-ItemProperty -Path 'HKLM:\SOFTWARE\OpenSSH' -PropertyType String `
-    -Name DefaultShell -Value 'C:\Program Files\PowerShell\7\pwsh.exe' -Force
-}
-
-function Install-WSL {
-  # Get-WindowsOptionalFeature -Online
-  Enable-WindowsOptionalFeature -Online -All -NoRestart -FeatureName `
-  @('VirtualMachinePlatform', 'Microsoft-Windows-Subsystem-Linux') | Out-Null
-
-  New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.wslconfig" `
-    -Target $(Resolve-Path -LiteralPath .\configs\wslconfig) -Force
-}
-
 function Install-dotNet {
   pwsh -NoProfile -ExecutionPolicy Unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Channel Current"
 }
--- windows/install.ps1
+++ windows/install.ps1
@@ -1,7 +1,7 @@
 Import-Module -Name .\common.psm1
 
 # Self elevate administrative permissions in this script
-if ( -not (Assert-Elevated)) {
+if (-not (Assert-Elevated)) {
   Start-Process pwsh "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb 'RunAs'
   exit
 }
@@ -40,6 +40,36 @@ function Install-Packages {
   }
 }
 
+function Install-OMP {
+  @(
+    'oh-my-posh',
+    'posh-git',
+    'Terminal-Icons'
+  ) | ForEach-Object {
+    Install-Module -Name $_ -Scope CurrentUser -Force
+  }
+}
+
+function Install-OpenSSH {
+  # https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse
+  Add-WindowsCapability -Online -Name OpenSSH.Client
+  Get-Service ssh-agent | Set-Service -StartupType Automatic -PassThru | Start-Service
+
+  Add-WindowsCapability -Online -Name OpenSSH.Server
+  Get-Service sshd | Set-Service -StartupType Automatic -PassThru | Start-Service
+
+  # Confirm the Firewall rule is configured. It should be created automatically by setup
+  if (-not (Get-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+  }
+}
+
+function Install-WSL {
+  # Get-WindowsOptionalFeature -Online
+  Enable-WindowsOptionalFeature -Online -All -NoRestart -FeatureName `
+  @('VirtualMachinePlatform', 'Microsoft-Windows-Subsystem-Linux') | Out-Null
+}
+
 function Install-Configs {
   $ProfileDir = Split-Path -Parent $Profile
   $PluginsDir = Join-Path $ProfileDir 'plugins'
@@ -59,15 +89,38 @@ function Install-Configs {
       New-Item -ItemType SymbolicLink -Path "$ProfileDir\.gnupg\$($_.Name)" `
         -Target $_.FullName -Force
     }
+
+  # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles
+  Get-ChildItem -Path '.\runcoms\*' -Include '*.ps1' |
+    ForEach-Object {
+      New-Item -ItemType SymbolicLink -Path "$ProfileDir\$($_.Name)" `
+        -Target $_.FullName -Force
+    }
+  Get-ChildItem -Path '.\runcoms\plugins\' |
+    ForEach-Object {
+      New-Item -ItemType SymbolicLink -Path "$PluginsDir\$($_.Name)" `
+        -Target $_.FullName -Force
+    }
+
+  New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.ssh\config" `
+    -Target $(Resolve-Path -LiteralPath .\configs\ssh_config) -Force
+  # icacls.exe "C:\ProgramData\ssh\administrators_authorized_keys" /inheritance:r /grant "Administrators:F" /grant "SYSTEM:F"
+  New-Item -ItemType SymbolicLink -Path "$env:ProgramData\ssh\sshd_config" `
+    -Target $(Resolve-Path -LiteralPath .\configs\sshd_config) -Force
+  New-ItemProperty -Path 'HKLM:\SOFTWARE\OpenSSH' -PropertyType String `
+    -Name DefaultShell -Value 'C:\Program Files\PowerShell\7\pwsh.exe' -Force
+
+  New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.wslconfig" `
+    -Target $(Resolve-Path -LiteralPath .\configs\wslconfig) -Force
 }
 
 function main {
   Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
   Install-Packages
-  Install-Configs
   Install-OpenSSH
   Install-OMP
   Install-WSL
+  Install-Configs
 }
 
 main
