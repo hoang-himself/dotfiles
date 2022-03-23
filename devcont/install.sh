7fddc61a87bbfdd1e29db9101319e133880f18e4
Hoang NGUYEN <baauco@gmail.com>
2022-03-23 21:55:17 +0700

    ⚰️ devcont(conf): Remove dangling configs

--- devcont/install.sh
+++ devcont/install.sh
@@ -1,19 +1,8 @@
 #!/usr/bin/env bash
 
 function link_dotfiles {
-  mkdir -p "$HOME"/.{gnupg,ssh}
-  ln -frs ./configs/ssh/config ~/.ssh/config
-
   for rcfile in ./runcom/*; do
     ln -frs "$rcfile" "$HOME/.$(basename "$rcfile")"
-  done
-
-  for rcfile in ../common/git/*; do
-    ln -frs "$rcfile" "$HOME/.$(basename "$rcfile")"
-  done
-
-  for rcfile in ../common/gnupg/*; do
-    ln -frs "$rcfile" "$HOME/.gnupg/$(basename "$rcfile")"
   done
 }
 
