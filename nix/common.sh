f69b4e605bafb1bd29d0d6db31ea07e49134f13c
Hoang NGUYEN <baauco@gmail.com>
2022-03-23 22:24:12 +0700

    📝 nix(conf): Update helper functions for Docker

--- nix/common.sh
+++ nix/common.sh
@@ -48,16 +48,21 @@ function install_static_cmake {
 }
 
 function install_docker_compose {
-  # "Install Docker Engine: https://docs.docker.com/engine/install/"
-  # "Run Dockerd as non-root: https://docs.docker.com/engine/security/rootless/"
-  # "Manage Dockerd as non-root: https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user"
+  # Install Docker Engine: https://docs.docker.com/engine/install/
+  # curl -fSL https://get.docker.com | bash
+
+  # Run Dockerd as non-root: https://docs.docker.com/engine/security/rootless/
+  # sudo "$1" install -y uidmap
+  # dockerd-rootless-setuptool.sh install
+
+  # Manage Dockerd as non-root: https://docs.docker.com/engine/install/linux-postinstall/#manage-docker-as-a-non-root-user
+  # sudo usermod -aG docker $(whoami)
 
   # local compose_version=$(curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-  # curl -SL "https://github.com/docker/compose/releases/download/${compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o ~/.docker/cli-plugins/docker-compose
+  # curl -fSL "https://github.com/docker/compose/releases/download/${compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o ~/.docker/cli-plugins/docker-compose
 
   mkdir -p ~/.docker/cli-plugins
-  curl -SL "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o ~/.docker/cli-plugins/docker-compose
-
+  curl -fSL "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o ~/.docker/cli-plugins/docker-compose
   chmod +x ~/.docker/cli-plugins/docker-compose
 }
 
