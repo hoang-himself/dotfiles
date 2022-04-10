c62bc6978d989af103fc37f4c1c159b65293a66a
Hoang NGUYEN <baauco@gmail.com>
2022-04-10 22:52:16 +0700

    🔨 Fix AvoidUsingInvokeExpression

--- windows/runcoms/profile.ps1
+++ windows/runcoms/profile.ps1
@@ -2,16 +2,22 @@ Import-Module -Name 'posh-git'
 Import-Module -Name 'oh-my-posh'
 Import-Module -Name 'Terminal-Icons'
 
-# oh-my-posh prompt init pwsh --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/v$(oh-my-posh --version)/themes/jandedobbeleer.omp.json" | Invoke-Expression
-oh-my-posh prompt init pwsh --config "$env:LOCALAPPDATA\oh-my-posh\themes\jandedobbeleer.omp.json" | Invoke-Expression
-oh-my-posh completion powershell | Out-String | Invoke-Expression
+#Invoke-Command -ScriptBlock $([ScriptBlock]::Create(
+#    $(oh-my-posh prompt init pwsh --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/v$(oh-my-posh --version)/themes/jandedobbeleer.omp.json")
+#  ))
+Invoke-Command -ScriptBlock $([ScriptBlock]::Create(
+    $(oh-my-posh prompt init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json")
+  ))
+Invoke-Command -ScriptBlock $([ScriptBlock]::Create(
+    $(oh-my-posh completion powershell)
+  ))
 
 # https://ohmyposh.dev/docs/git
 $env:POSH_GIT_ENABLED = $true
 
 # https://docs.microsoft.com/en-us/powershell/module/psreadline/about/about_psreadline
-Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
+Set-PSReadLineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 
 Push-Location (Split-Path -Parent $Profile)
-@('plugins', 'functions', 'aliases', 'exports', 'extra') | Where-Object { Test-Path "$_.ps1" } | ForEach-Object -Process { Invoke-Expression ". .\$_.ps1" }
+@('plugins', 'functions', 'aliases', 'exports', 'extra') | Where-Object { Test-Path "$_.ps1" } | ForEach-Object -Process { . ".\$_.ps1" }
 Pop-Location
