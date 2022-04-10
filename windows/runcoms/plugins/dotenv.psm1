bf6e4fec98e5abc858381acdec5ef88277e31b2c
Hoang NGUYEN <baauco@gmail.com>
2022-04-10 21:28:58 +0700

    🔨 Fix AvoidGlobalVars and ProcessBlockForPipeline

--- windows/runcoms/plugins/dotenv.psm1
+++ windows/runcoms/plugins/dotenv.psm1
@@ -31,66 +31,67 @@ function Set-DotEnv {
   [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Low')]
   param(
     [Parameter(Mandatory = $false, Position = 0, ValueFromPipeline = $true)]
-    [string]
-    $LocalEnvFile = '.env'
+    [String]$LocalEnvFile = '.env'
   )
 
-  if ($Global:PreviousDir -eq (Get-Location).Path) {
-    Write-Verbose 'Set-DotEnv: Skipping same dir'
-    return
-  }
-  else {
-    $Global:PreviousDir = (Get-Location).Path
-  }
-
-  # Return if no env file
-  if (-not (Test-Path $LocalEnvFile)) {
-    Write-Verbose "No $LocalEnvFile file"
-    return
-  }
-
-  # Read the local env file
-  $content = Get-Content $LocalEnvFile -ErrorAction Stop
-  Write-Verbose "Parsed $LocalEnvFile file"
-
-  # Load the content to environment
-  foreach ($line in $content) {
-
-    if ([string]::IsNullOrWhiteSpace($line)) {
-      Write-Verbose 'Skipping empty line'
-      continue
-    }
-
-    # Ignore comments
-    if ($line.StartsWith('#')) {
-      Write-Verbose "Skipping comment line $line"
-      continue
-    }
-
-    # Prefix operator
-    if ($line -like '*:=*') {
-      Write-Verbose 'Prefix'
-      $kvp = $line -split @(':=', 2)
-      $key = $kvp[0].Trim()
-      $value = '{0};{1}' -f $kvp[1].Trim(), [System.Environment]::GetEnvironmentVariable($key)
-    }
-    elseif ($line -like '*=:*') {
-      Write-Verbose 'Suffix'
-      $kvp = $line -split @('=:', 2)
-      $key = $kvp[0].Trim()
-      $value = '{1};{0}' -f $kvp[1].Trim(), [System.Environment]::GetEnvironmentVariable($key)
+  process {
+    if ($PreviousDir -eq (Get-Location).Path) {
+      Write-Verbose 'Set-DotEnv: Skipping same dir'
+      return
     }
     else {
-      Write-Verbose 'Assign'
-      $kvp = $line -split @('=', 2)
-      $key = $kvp[0].Trim()
-      $value = $kvp[1].Trim()
+      $PreviousDir = (Get-Location).Path
     }
 
-    Write-Verbose "$key=$value"
+    # Return if no env file
+    if (-not (Test-Path $LocalEnvFile)) {
+      Write-Verbose "No $LocalEnvFile file"
+      return
+    }
 
-    if ($PSCmdlet.ShouldProcess("Variable $key", "Value $value")) {
-      [Environment]::SetEnvironmentVariable($key, $value, 'Process') | Out-Null
+    # Read the local env file
+    $content = Get-Content $LocalEnvFile -ErrorAction Stop
+    Write-Verbose "Parsed $LocalEnvFile file"
+
+    # Load the content to environment
+    foreach ($line in $content) {
+
+      if ([string]::IsNullOrWhiteSpace($line)) {
+        Write-Verbose 'Skipping empty line'
+        continue
+      }
+
+      # Ignore comments
+      if ($line.StartsWith('#')) {
+        Write-Verbose "Skipping comment line $line"
+        continue
+      }
+
+      # Prefix operator
+      if ($line -like '*:=*') {
+        Write-Verbose 'Prefix'
+        $kvp = $line -split @(':=', 2)
+        $key = $kvp[0].Trim()
+        $value = '{0};{1}' -f $kvp[1].Trim(), [System.Environment]::GetEnvironmentVariable($key)
+      }
+      elseif ($line -like '*=:*') {
+        Write-Verbose 'Suffix'
+        $kvp = $line -split @('=:', 2)
+        $key = $kvp[0].Trim()
+        $value = '{1};{0}' -f $kvp[1].Trim(), [System.Environment]::GetEnvironmentVariable($key)
+      }
+      else {
+        Write-Verbose 'Assign'
+        $kvp = $line -split @('=', 2)
+        $key = $kvp[0].Trim()
+        $value = $kvp[1].Trim()
+      }
+
+      Write-Verbose "$key=$value"
+
+      if ($PSCmdlet.ShouldProcess("Variable $key", "Value $value")) {
+        [Environment]::SetEnvironmentVariable($key, $value, 'Process') | Out-Null
+      }
     }
   }
 }
