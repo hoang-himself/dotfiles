b873a5906be6870186a782fd42d00158e83aa417
Hoang NGUYEN <baauco@gmail.com>
2022-04-13 12:32:35 +0700

    ⚰️ Drop unneeded helper function

--- linux/helpers.sh
+++ linux/helpers.sh
@@ -23,18 +23,3 @@ function cleantmp {
   # Need to add sudo here because git clones leave behind write-protected files
   command sudo rm -r "$TMP_DIR"
 }
-
-function _dangerous {
-  if [[ $UID -eq 0 ]]; then
-    echo "Looks like you are running this script as root"
-    echo "Please run this script without root or sudo"
-    echo "We will ask for your password when needed"
-    exit 1
-  fi
-
-  echo "$(whoami) ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/00-$(whoami)" >/dev/null
-  sudo chmod 0440 "/etc/sudoers.d/00-$(whoami)"
-
-  echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOlzVupDIQTLHJibTuOt+mcrRVY35b9yFn0SrAq5cCZ3 baauco@gmail.com' | sudo tee -a "/etc/ssh/keys/$(whoami)/authorized_keys" >/dev/null
-  sudo chmod 0600 "/etc/ssh/keys/$(whoami)/authorized_keys"
-}
