9a91334fcf1f2b885e5a54836e31585ae92b51ac
Hoang NGUYEN <baauco@gmail.com>
2022-04-27 22:07:51 +0700

    🔧 Add starship and remove node

--- windows/legacy.psm1
+++
@@ -1,8 +0,0 @@
-function Install-dotNet {
-  pwsh -NoProfile -ExecutionPolicy Unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -Channel Current"
-}
-
-function _dangerous {
-  Set-Content -Path "$env:ProgramData\ssh\keys\$env:USERNAME\authorized_keys" `
-    -Value 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOlzVupDIQTLHJibTuOt+mcrRVY35b9yFn0SrAq5cCZ3 baauco@gmail.com'
-}
