a93b29977da9520f0eaad7e3fc44aeff7bb7dde3
Hoang NGUYEN <baauco@gmail.com>
2022-07-14 11:17:25 +0700

    Move buildx install script to raspberrypi repo
    because Docker sucks at naming binaries

--- linux/bootstrap.sh
+++ linux/bootstrap.sh
@@ -18,12 +18,3 @@ function install_docker_compose {
     -o "$HOME/.docker/cli-plugins/docker-compose"
   chmod +x "$HOME/.docker/cli-plugins/docker-compose"
 }
-
-function install_docker_buildx {
-  mkdir -p "$HOME/.docker/cli-plugins"
-  buildx_version=$(curl --silent "https://api.github.com/repos/docker/buildx/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-  # TODO might move this to ~/raspberrypi because uname -m returns aarch64 instead of arm64
-  curl -fSL "https://github.com/docker/buildx/releases/download/${buildx_version}/buildx-${buildx_version}.linux-arm64" \
-    -o "$HOME/.docker/cli-plugins/docker-buildx"
-  chmod +x "$HOME/.docker/cli-plugins/docker-buildx"
-}
