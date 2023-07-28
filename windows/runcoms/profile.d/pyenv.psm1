67373f23b3cea45ea59f15162458f3b55ce82956
Hoang NGUYEN <baauco@gmail.com>
2023-07-29 00:57:55 +0700

    refactor(windows): refactor call operators

--- windows/runcoms/profile.d/pyenv.psm1
+++ windows/runcoms/profile.d/pyenv.psm1
@@ -4,18 +4,18 @@ function va {
   $venvPath = if ($args) { $args } else { '.venv' }
 
   if ((Get-Item -Path "$venvPath" -ErrorAction SilentlyContinue).PSIsContainer) {
-    & ".\$venvPath\Scripts\Activate.ps1"
+    &".\$venvPath\Scripts\Activate.ps1"
     return
   }
 
   python -m venv --upgrade-deps "$venvPath"
-  & ".\$venvPath\Scripts\Activate.ps1"
+  &".\$venvPath\Scripts\Activate.ps1"
   # --upgrade-deps already includes pip and setuptools
-  & ".\$venvPath\Scripts\pip.exe" install wheel
+  &".\$venvPath\Scripts\pip.exe" install wheel
 
   $requirementsFile = 'requirements.txt'
   if (Test-Path -Path "$requirementsFile" -PathType Leaf) {
-    & ".\$venvPath\Scripts\pip.exe" install -r "$requirementsFile"
+    &".\$venvPath\Scripts\pip.exe" install -r "$requirementsFile"
   } else {
     Add-Content -Path "$requirementsFile" -Value $null
   }
--- windows/runcoms/profile.d/vscode.psm1
+++ windows/runcoms/profile.d/vscode.psm1
@@ -21,19 +21,19 @@ if (-not "$env:VSCODE") {
   return
 }
 
-${function:vsc} = { & "$env:VSCODE" $args }
-${function:vsca} = { & "$env:VSCODE" --add $args }
-${function:vscd} = { & "$env:VSCODE" --diff $args }
-${function:vscg} = { & "$env:VSCODE" --goto $args }
-${function:vscn} = { & "$env:VSCODE" --new-window $args }
-${function:vscr} = { & "$env:VSCODE" --reuse-window $args }
-${function:vscw} = { & "$env:VSCODE" --wait $args }
-${function:vscu} = { & "$env:VSCODE" --user-data-dir $args }
+${function:vsc} = { &"$env:VSCODE" $args }
+${function:vsca} = { &"$env:VSCODE" --add $args }
+${function:vscd} = { &"$env:VSCODE" --diff $args }
+${function:vscg} = { &"$env:VSCODE" --goto $args }
+${function:vscn} = { &"$env:VSCODE" --new-window $args }
+${function:vscr} = { &"$env:VSCODE" --reuse-window $args }
+${function:vscw} = { &"$env:VSCODE" --wait $args }
+${function:vscu} = { &"$env:VSCODE" --user-data-dir $args }
 
-${function:vsced} = { & "$env:VSCODE" --extensions-dir $args }
-${function:vscie} = { & "$env:VSCODE" --install-extension $args }
-${function:vscue} = { & "$env:VSCODE" --uninstall-extension $args }
+${function:vsced} = { &"$env:VSCODE" --extensions-dir $args }
+${function:vscie} = { &"$env:VSCODE" --install-extension $args }
+${function:vscue} = { &"$env:VSCODE" --uninstall-extension $args }
 
-${function:vscv} = { & "$env:VSCODE" --verbose $args }
-${function:vscl} = { & "$env:VSCODE" --log $args }
-${function:vscde} = { & "$env:VSCODE" --disable-extensions $args }
+${function:vscv} = { &"$env:VSCODE" --verbose $args }
+${function:vscl} = { &"$env:VSCODE" --log $args }
+${function:vscde} = { &"$env:VSCODE" --disable-extensions $args }
