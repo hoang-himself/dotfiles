61fa70a1514c50311b46bd55e006e258eea8d425
Hoang NGUYEN <baauco@gmail.com>
2024-02-25 01:29:27 +0700

    docs(ventoy/winnt10): prepare office installation image

--- ventoy/bootstrap/WinNT10/Scripts/SetupComplete.cmd
+++ ventoy/bootstrap/WinNT10/Scripts/SetupComplete.cmd
@@ -1,4 +1,4 @@
-:: Precheck
+:: Precondition
 fltmc >nul || exit /b
 
 :: Set paths
@@ -10,15 +10,13 @@ set SCRIPTSDIR=%SETUPDIR%\Scripts
 "%ASSETSDIR%\7z-x64.exe" /S
 
 :: Office
-:: In start_setup.cmd, use start -wait setup.exe
-:: In configure.xml, use <Display Level="None" AcceptEULA="Yes" />
 start /wait cmd /c "%ASSETSDIR%\Office\start_setup.cmd"
 
 ::
 curl -SL -o "%SCRIPTSDIR%\MAS_AIO.cmd" https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_AIO.cmd
 start /wait cmd /c "%SCRIPTSDIR%\MAS_AIO.cmd" /HWID /Ohook
 
-:: Clean up
+:: Postcondition
 if exist "%SETUPDIR%\CreateSetup.cmd" @rd /S /Q "%SETUPDIR%\CreateSetup.cmd"
 if exist "%ASSETSDIR%" @rd /S /Q "%ASSETSDIR%"
 if exist "%SCRIPTSDIR%" @rd /S /Q "%SCRIPTSDIR%"
