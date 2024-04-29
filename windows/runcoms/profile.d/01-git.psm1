465747c634cd00b1cc7f09ff6b293360acfaf019
Hoang NGUYEN <baauco@gmail.com>
2024-04-29 23:28:22 +0700

    feat(windows): sync `git` runcoms

--- windows/runcoms/profile.d/01-git.psm1
+++ windows/runcoms/profile.d/01-git.psm1
@@ -1,3 +1,8 @@
+# The git prompt's git commands are read-only and should not interfere with
+# other processes. This environment variable is equivalent to running with `git
+# --no-optional-locks`, but falls back gracefully for older versions of git.
+# See git(1) for and git-status(1) for a description of that flag.
+
 # We wrap in a local function instead of exporting the variable directly in
 # order to avoid interfering with manually-run git commands by the user.
 function __git_prompt_git {
@@ -15,19 +20,20 @@ function git_current_branch {
 # Outputs the name of the current user
 # Usage example: $(git_current_user_name)
 function git_current_user_name {
-  __git_prompt_git config user.name 2>&1 | Out-Null
+  __git_prompt_git config user.name
 }
 
 # Outputs the email of the current user
 # Usage example: $(git_current_user_email)
 function git_current_user_email {
-  __git_prompt_git config user.email 2>&1 | Out-Null
+  __git_prompt_git config user.email
 }
 
 # Output the name of the root directory of the git repository
 # Usage example: $(git_repo_name)
 function git_repo_name {
-  if ($repo_path = $(__git_prompt_git rev-parse --show-toplevel 2>&1 | Out-Null)) {
-    return $repo_path.Split('/')[-1]
+  $repo_path = __git_prompt_git rev-parse --show-toplevel
+  if ($repo_path) {
+    return Split-Path $repo_path -Leaf
   }
 }
--- windows/runcoms/profile.d/10-git.psm1
+++ windows/runcoms/profile.d/10-git.psm1
@@ -9,7 +9,7 @@ function git_develop_branch {
   $git_dir = git rev-parse --git-dir 2>&1
   if ($git_dir -ne '.git') { return }
 
-  $refs = git show-ref | ForEach-Object -Process { $_.Split(' ')[-1] }
+  $refs = git show-ref | ForEach-Object -Process { $_ -split ' ' | Select-Object -Last 1 }
   $develop_refs = @('refs/heads', 'refs/remotes/origin', 'refs/remotes/upstream') | ForEach-Object -Process {
     $i = $_
     @('develop', 'dev', 'devel', 'development') | ForEach-Object -Process { "$i/$_" }
@@ -17,7 +17,7 @@ function git_develop_branch {
 
   ForEach ($ref in $refs) {
     if ($develop_refs -contains $ref) {
-      return $ref.Split('/')[-1]
+      return $ref -split '/' | Select-Object -Last 1
     }
   }
   return 'develop'
@@ -28,7 +28,7 @@ function git_main_branch {
   $git_dir = git rev-parse --git-dir 2>&1
   if ($git_dir -ne '.git') { return }
 
-  $refs = git show-ref | ForEach-Object -Process { $_.Split(' ')[-1] }
+  $refs = git show-ref | ForEach-Object -Process { $_ -split ' ' | Select-Object -Last 1 }
   $main_refs = @('refs/heads', 'refs/remotes/origin', 'refs/remotes/upstream') | ForEach-Object -Process {
     $i = $_
     @('main', 'trunk', 'mainline', 'default') | ForEach-Object -Process { "$i/$_" }
@@ -36,23 +36,25 @@ function git_main_branch {
 
   ForEach ($ref in $refs) {
     if ($main_refs -contains $ref) {
-      return $ref.Split('/')[-1]
+      return $ref -split '/' | Select-Object -Last 1
     }
   }
   return 'master'
 }
 
 function grename {
-  if (-not ([bool]$args[0] && [bool]$args[1])) {
-    Write-Output -InputObject "Usage: $((Get-PSCallStack).FunctionName[0]) old_branch new_branch"
-    return 1
-  }
+  param(
+    [Parameter(Mandatory = $true)]
+    [string]$old_branch,
+    [Parameter(Mandatory = $true)]
+    [string]$new_branch
+  )
 
   # Rename branch locally
-  git branch -m $args[0] $args[1]
+  git branch -m $old_branch $new_branch
   # Rename branch in origin remote
-  if (git push origin :$args[1]) {
-    git push --set-upstream origin $args[1]
+  if (git push origin :$new_branch) {
+    git push --set-upstream origin $new_branch
   }
 }
 
@@ -64,17 +66,17 @@ function grename {
 
 # Similar to `gunwip` but recursive "Unwips" all recent `--wip--` commits not just the last one
 function gunwipall {
-  $commit = $(git log --grep='--wip--' --invert-grep --max-count=1 --format=format:%H)
+  $_commit = git log --grep='--wip--' --invert-grep --max-count=1 --format=format:%H
 
   # Check if a commit without "--wip--" was found and it's not the same as HEAD
-  if ("$commit" -ne "$(git rev-parse HEAD)") {
-    git reset "$commit" || return 1
+  if ("$_commit" -ne "$(git rev-parse HEAD)") {
+    git reset "$_commit" || Write-Error "Failed to reset to $_commit"
   }
 }
 
 # Warn if the current branch is a WIP
 function work_in_progress {
-  if (git -c log.showSignature=false log -n 1 2>&1 | Select-String -Pattern '--wip--' -Quiet) { return 'WIP!!' }
+  if ((git -c log.showSignature=false log --oneline -n 1 2>&1) -match '--wip--') { return 'WIP!!' }
 }
 
 #
@@ -93,10 +95,17 @@ Remove-Alias -Name 'gm' -Force
 Remove-Alias -Name 'gp' -Force
 Remove-Alias -Name 'gpv' -Force
 
-${function:grt} = { Set-Location -Path "$(git rev-parse --show-toplevel || Write-Output -InputObject .)" }
+function grt {
+  $git_top_level = git rev-parse --show-toplevel 2>&1
+  if ($LASTEXITCODE -eq 0) {
+    Set-Location -Path $git_top_level
+  } else {
+    Write-Error 'Failed to get the top-level directory'
+  }
+}
 
 function ggpnp {
-  if ($args.count -eq 0) {
+  if ($args.Count -eq 0) {
     ggl && ggp
   } else {
     ggl $args && ggp $args
@@ -111,20 +120,18 @@ ${function:gapa} = { git add --patch $args }
 ${function:gau} = { git add --update $args }
 ${function:gav} = { git add --verbose $args }
 ${function:gwip} = {
-  git add -A;
-  git rm $(git ls-files --deleted) 2>&1 | Out-Null;
+  git add --all
   git commit --no-verify --no-gpg-sign -m '--wip-- [skip ci]'
 }
 ${function:gam} = { git am $args }
 ${function:gama} = { git am --abort }
 ${function:gamc} = { git am --continue }
-${function:gamscp} = { git am --show-current-patch }
+${function:gamscp} = { git am --show-current-patch $args }
 ${function:gams} = { git am --skip }
 ${function:gap} = { git apply $args }
 ${function:gapt} = { git apply --3way $args }
 ${function:gbs} = { git bisect $args }
 ${function:gbsb} = { git bisect bad $args }
-${function:gbsg} = { git bisect good $args }
 ${function:gbsg} = { git bisect good $args }
 ${function:gbsn} = { git bisect new $args }
 ${function:gbso} = { git bisect old $args }
@@ -135,36 +142,54 @@ ${function:gb} = { git branch $args }
 ${function:gba} = { git branch --all $args }
 ${function:gbd} = { git branch --delete $args }
 ${function:gbD} = { git branch --delete --force $args }
+
 ${function:gbda} = {
   git branch --delete @(
     git branch --no-color --merged `
-    | Select-String -NotMatch -Pattern "^([+*]|\s*($(git_main_branch)|$(git_develop_branch))\s*$)" `
-    | ForEach-Object -Process { $_.Line.Trim() }
+    | ForEach-Object -Process { $_ -split '\s' } `
+    | Where-Object { $_ -notmatch "^([+*]|($(git_main_branch)|$(git_develop_branch))$)" }
   ) 2>&1 | Out-Null
 }
+${function:gbds} = {
+  $default_branch = git_main_branch
+  if ($LASTEXITCODE -ne 0) {
+    $default_branch = git_develop_branch
+  }
+
+  git for-each-ref refs/heads/ '--format=%(refname:short)' | ForEach-Object {
+    $branch = $_
+    $merge_base = git merge-base $default_branch $branch
+    $commit_tree = git commit-tree (git rev-parse "$branch\^ { tree }") -p $merge_base -m '_'
+    if ((git cherry $default_branch $commit_tree) -like '-*') {
+      git branch -D $branch
+    }
+  }
+}
 ${function:gbgd} = {
+  $env:LANG = 'C'
   git branch -d @(
     git branch --no-color -vv `
     | Select-String -Pattern ': gone\]' `
-    | ForEach-Object -Process { ($_ -split '\s+')[1] }
+    | ForEach-Object -Process { $_ -split '\s+' | Select-Object -Index 1 }
   )
 }
 ${function:gbgD} = {
+  $env:LANG = 'C'
   git branch -D @(
     git branch --no-color -vv `
     | Select-String -Pattern ': gone\]' `
-    | ForEach-Object -Process { ($_ -split '\s+')[1] }
+    | ForEach-Object -Process { $_ -split '\s+' | Select-Object -Index 1 }
   )
 }
 ${function:gbnm} = { git branch --no-merged $args }
 ${function:gbr} = { git branch --remote $args }
-${function:ggsup} = { git branch --set-upstream-to="origin/$(git_current_branch)" }
+${function:ggsup} = { git branch --set-upstream-to="origin/$(git_current_branch)" $args }
 ${function:gbg} = { git branch -vv | Select-String -Pattern ': gone\]' }
 ${function:gco} = { git checkout $args }
 ${function:gcor} = { git checkout --recurse-submodules $args }
 ${function:gcb} = { git checkout -b $args }
-${function:gcd} = { git checkout $(git_develop_branch) }
-${function:gcm} = { git checkout $(git_main_branch) }
+${function:gcd} = { git checkout $args $(git_develop_branch) }
+${function:gcm} = { git checkout $args $(git_main_branch) }
 ${function:gcp} = { git cherry-pick $args }
 ${function:gcpa} = { git cherry-pick --abort }
 ${function:gcpc} = { git cherry-pick --continue }
@@ -172,12 +197,25 @@ ${function:gclean} = { git clean --interactive -d $args }
 ${function:gcl} = { git clone --recurse-submodules $args }
 
 function gccd {
-  git clone --recurse-submodules $args
-  if (Test-Path -Path $args) {
-    Set-Location -Path $args
-  } else {
-    Set-Location -Path $args.Split('/')[-1]
+  param(
+    [Parameter(Mandatory = $true)]
+    [string]$repo
+  )
+
+  # Clone repository and exit if it fails
+  git clone --recurse-submodules $repo
+  if ($LASTEXITCODE -ne 0) {
+    Write-Error 'Failed to clone repository'
+    return
   }
+
+  # Get the name of the repository
+  $repoName = Split-Path $repo -Leaf
+  # Remove .git extension if it exists
+  $repoName = $repoName -replace '\.git$', ''
+
+  # Change directory to the cloned repository
+  Set-Location -Path $repoName
 }
 
 ${function:gcam} = { git commit --all --message $args }
@@ -203,7 +241,7 @@ ${function:gdcw} = { git diff --cached --word-diff $args }
 ${function:gds} = { git diff --staged $args }
 ${function:gdw} = { git diff --word-diff $args }
 
-function gdv { git diff -w $args | &"$env:EDITOR" -R - }
+${function:gdv} = { git diff -w $args | &"$env:EDITOR" -R - }
 
 ${function:gdup} = { git diff '@{upstream}' $args }
 
@@ -233,7 +271,7 @@ ${function:gloga} = { git log --oneline --decorate --graph --all $args }
 # Pretty log messages
 function _git_log_prettily {
   if ($args[0]) {
-    git log --pretty=$args[0]
+    git log --pretty=$args
   }
 }
 
@@ -245,67 +283,99 @@ ${function:gfg} = { git ls-files | Select-String -Pattern $args }
 ${function:gm} = { git merge $args }
 ${function:gma} = { git merge --abort }
 ${function:gms} = { git merge --squash $args }
-${function:gmom} = { git merge "origin/$(git_main_branch)" }
-${function:gmum} = { git merge "upstream/$(git_main_branch)" }
+${function:gmom} = { git merge $args "origin/$(git_main_branch)" }
+${function:gmum} = { git merge $args "upstream/$(git_main_branch)" }
 ${function:gmtl} = { git mergetool --no-prompt $args }
 ${function:gmtlvim} = { git mergetool --no-prompt --tool='nvim -d' $args }
+
 ${function:gl} = { git pull $args }
 ${function:gpr} = { git pull --rebase $args }
-${function:gup} = { git pull --rebase $args }
-${function:gupa} = { git pull --rebase --autostash $args }
-${function:gupav} = { git pull --rebase --autostash --verbose $args }
-${function:gupv} = { git pull --rebase --verbose $args }
+${function:gprv} = { git pull --rebase --verbose $args }
+${function:gpra} = { git pull --rebase --autostash $args }
+${function:gprav} = { git pull --rebase --autostash --verbose $args }
 
 function ggu {
-  $b = if ($args.count -ne 1) { git_current_branch } else { $args[0] }
-  git pull --rebase origin "$b"
+  param(
+    [Parameter(Mandatory = $false)]
+    [string]$branch
+  )
+
+  if (-not $branch) {
+    $branch = git_current_branch
+  }
+
+  git pull --rebase origin $branch
 }
 
-${function:gupom} = { git pull --rebase origin $(git_main_branch) }
-${function:gupomi} = { git pull --rebase=interactive origin $(git_main_branch) }
+${function:gprom} = { git pull --rebase origin $(git_main_branch) }
+${function:gpromi} = { git pull --rebase=interactive origin $(git_main_branch) }
 ${function:ggpull} = { git pull origin $(git_current_branch) }
 
 function ggl {
-  if (($args.count -ne 0) -and ($args.count -ne 1)) {
-    git pull origin $args
-  } else {
-    $b = if ($args.count -eq 0) { git_current_branch } else { $args[0] }
-    git pull origin "$b"
+  param(
+    [Parameter(Mandatory = $false)]
+    [string]$branch
+  )
+
+  if (-not $branch) {
+    $branch = git_current_branch
   }
+
+  git pull origin $branch
 }
 
-${function:gluc} = { git pull upstream $(git_current_branch) }
-${function:glum} = { git pull upstream $(git_main_branch) }
+${function:gluc} = { git pull $args upstream $(git_current_branch) }
+${function:glum} = { git pull $args upstream $(git_main_branch) }
 ${function:gp} = { git push $args }
 ${function:gpd} = { git push --dry-run $args }
 
 function ggf {
-  $b = if ($args.count -ne 1) { git_current_branch } else { $args[0] }
-  git push --force origin "$b"
+  param(
+    [Parameter(Mandatory = $false)]
+    [string]$branch
+  )
+
+  if (-not $branch) {
+    $branch = git_current_branch
+  }
+
+  git push --force origin $branch
 }
 
 ${function:gpf!} = { git push --force $args }
 ${function:gpf} = { git push --force-with-lease --force-if-includes $args }
 
 function ggfl {
-  $b = if ($args.count -ne 1) { git_current_branch } else { $args[0] }
-  git push --force-with-lease origin "$b"
+  param(
+    [Parameter(Mandatory = $false)]
+    [string]$branch
+  )
+
+  if (-not $branch) {
+    $branch = git_current_branch
+  }
+
+  git push --force-with-lease origin $branch
 }
 
-${function:gpsup} = { git push --set-upstream origin $(git_current_branch) }
-${function:gpsupf} = { git push --set-upstream origin $(git_current_branch) --force-with-lease --force-if-includes }
+${function:gpsup} = { git push $args --set-upstream origin $(git_current_branch) }
+${function:gpsupf} = { git push $args --set-upstream origin $(git_current_branch) --force-with-lease --force-if-includes }
 ${function:gpv} = { git push --verbose $args }
-${function:gpoat} = { git push origin --all && git push origin --tags }
-${function:gpod} = { git push origin --delete $args }
-${function:ggpush} = { git push origin $(git_current_branch) }
+${function:gpoat} = { git push --all origin && git push --tags origin }
+${function:gpod} = { git push $args --delete origin }
+${function:ggpush} = { git push $args origin $(git_current_branch) }
 
 function ggp {
-  if (($args.count -ne 0) -and ($args.count -ne 1)) {
-    git push origin $args
-  } else {
-    $b = if ($args.count -eq 0) { git_current_branch } else { $args[0] }
-    git push origin "$b"
+  param(
+    [Parameter(Mandatory = $false)]
+    [string]$branch
+  )
+
+  if (-not $branch) {
+    $branch = git_current_branch
   }
+
+  git push origin $branch
 }
 
 ${function:gpu} = { git push upstream $args }
@@ -315,9 +385,9 @@ ${function:grbc} = { git rebase --continue }
 ${function:grbi} = { git rebase --interactive $args }
 ${function:grbo} = { git rebase --onto $args }
 ${function:grbs} = { git rebase --skip }
-${function:grbd} = { git rebase $(git_develop_branch) }
-${function:grbm} = { git rebase $(git_main_branch) }
-${function:grbom} = { git rebase "origin/$(git_main_branch)" }
+${function:grbd} = { git rebase $args $(git_develop_branch) }
+${function:grbm} = { git rebase $args $(git_main_branch) }
+${function:grbom} = { git rebase $args "origin/$(git_main_branch)" }
 ${function:gr} = { git remote $args }
 ${function:grv} = { git remote --verbose $args }
 ${function:gra} = { git remote add $args }
@@ -328,12 +398,12 @@ ${function:grup} = { git remote update $args }
 ${function:grh} = { git reset $args }
 ${function:gru} = { git reset -- $args }
 ${function:grhh} = { git reset --hard $args }
-${function:gpristine} = { git reset --hard && git clean --force -dfx $args }
-${function:groh} = { git reset "origin/$(git_current_branch)" --hard $args }
+${function:gpristine} = { git reset --hard && git clean --force -dfx }
+${function:groh} = { git reset --hard "origin/$(git_current_branch)" }
 ${function:grs} = { git restore $args }
 ${function:grss} = { git restore --source $args }
 ${function:grst} = { git restore --staged $args }
-${function:gunwip} = { if (git log -n 1 | Select-String -Pattern '--wip--') { git reset '@~' } }
+${function:gunwip} = { if (git log -n 1 --pretty=%B | Select-String -Pattern '--wip--') { git reset HEAD~ } }
 ${function:grev} = { git revert $args }
 ${function:grm} = { git rm $args }
 ${function:grmc} = { git rm --cached $args }
@@ -342,7 +412,7 @@ ${function:gsh} = { git show $args }
 ${function:gsps} = { git show --pretty=short --show-signature $args }
 ${function:gstall} = { git stash --all $args }
 ${function:gstaa} = { git stash apply $args }
-${function:gstc} = { git stash clear $args }
+${function:gstc} = { git stash clear }
 ${function:gstd} = { git stash drop $args }
 ${function:gstl} = { git stash list $args }
 ${function:gstp} = { git stash pop $args }
@@ -361,7 +431,7 @@ ${function:gswc} = { git switch -c $args }
 ${function:gswm} = { git switch $(git_main_branch) }
 ${function:gswd} = { git switch $(git_develop_branch) }
 ${function:gts} = { git tag -s $args }
-${function:gtv} = { git tag | Sort-Object }
+${function:gtv} = { git tag --sort=-v:refname }
 ${function:gignore} = { git update-index --assume-unchanged $args }
 ${function:gunignore} = { git update-index --no-assume-unchanged $args }
 ${function:gwch} = { git whatchanged -p --abbrev-commit --pretty=medium $args }
@@ -373,4 +443,4 @@ ${function:gwtrm} = { git worktree remove $args }
 ${function:gstu} = { gsta --include-untracked $args }
 ${function:gtl} = { git tag --sort=-v:refname -n -l $args }
 ${function:gk} = { gitk --all --branches $args }
-${function:gke} = { gitk --all $(git log --walk-reflogs --pretty=%h) $args }
+${function:gke} = { gitk --all --reflog $args }
