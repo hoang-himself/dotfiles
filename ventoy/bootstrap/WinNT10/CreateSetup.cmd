a2ebeefba735786462c47f595df6a04b77623ab8
Hoang NGUYEN <baauco@gmail.com>
2024-10-01 22:52:58 +0700

    feat(ventoy/winnt10): cherry pick from unattended winstall

--- ventoy/bootstrap/WinNT10/CreateSetup.cmd
+++ ventoy/bootstrap/WinNT10/CreateSetup.cmd
@@ -5,5 +5,5 @@ set SETUPDIR=%WINDIR%\Setup
 set ASSETSDIR=%SETUPDIR%\Assets
 
 :: Copy files to local
-xcopy /herky "%FILEDIR%\*.*" "%SETUPDIR%\" >nul
+xcopy /HIVERKY "%FILEDIR%\*.*" "%SETUPDIR%\" 2>&1
 if not exist "%ASSETSDIR%" mkdir "%ASSETSDIR%"
