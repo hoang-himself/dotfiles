c5e3dbec35f28f5a3449f268291259927d1a34de
Hoang NGUYEN <baauco@gmail.com>
2024-10-24 19:46:15 +0700

    refactor(ventoy): convert commands to notes

--- ventoy/bootstrap/WinNT10/Scripts/SetupComplete.cmd
+++ ventoy/bootstrap/WinNT10/Scripts/SetupComplete.cmd
@@ -7,14 +7,15 @@ set ASSETSDIR=%SETUPDIR%\Assets
 set SCRIPTSDIR=%SETUPDIR%\Scripts
 
 :: 7-Zip
-"%ASSETSDIR%\7z-x64.exe" /S
+::curl -H "User-Agent: Wget x64" -SL -o "%ASSETSDIR%\7z-x64.exe" https://sourceforge.net/projects/sevenzip/files/latest/download
+::"%ASSETSDIR%\7z-x64.exe" /S
 
 :: Office
-start /wait cmd /c "%ASSETSDIR%\Office\start_setup.cmd"
+::start /wait cmd /c "%ASSETSDIR%\Office\start_setup.cmd"
 
 ::
-curl -SL -o "%SCRIPTSDIR%\MAS_AIO.cmd" https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd
-start /wait cmd /c "%SCRIPTSDIR%\MAS_AIO.cmd" /HWID /Ohook
+::curl -SL -o "%SCRIPTSDIR%\MAS_AIO.cmd" https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd
+::start /wait cmd /c "%SCRIPTSDIR%\MAS_AIO.cmd" /HWID /Ohook
 
 :: Postcondition
 if exist "%SETUPDIR%\CreateSetup.cmd" @rd /S /Q "%SETUPDIR%\CreateSetup.cmd"
