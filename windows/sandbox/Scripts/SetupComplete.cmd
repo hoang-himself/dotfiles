7b8d0f67f36d55eb5276e821636e0f27b500c81f
Hoang NGUYEN <baauco@gmail.com>
2025-01-05 23:02:35 +0700

    feat(windows/sandbox): add `wireguard`

--- windows/sandbox/Scripts/SetupComplete.cmd
+++ windows/sandbox/Scripts/SetupComplete.cmd
@@ -1,13 +1,15 @@
 set SETUPDIR=C:\Users\WDAGUtilityAccount\Desktop\Setup
 set ASSETSDIR=%TEMP%\Assets
 
-rem We need a writeable location to launch MSIs it seems
+:: We need a writeable location to launch MSIs for some reason
 xcopy /HIVERKYQ %SETUPDIR%\* %TEMP%\
 
-rem https://github.com/microsoft/vscode/blob/main/build/win32/code.iss
-"%ASSETSDIR%\vscode.exe" /silent /suppressmsgboxes /MERGETASKS="!runcode,desktopicon,quicklaunchicon,addcontextmenufiles,addcontextmenufolders,addtopath"
-"%ASSETSDIR%\7z-x64.exe" /S
+::
+"%ASSETSDIR%\7zip-installer.exe" /S
+:: https://github.com/microsoft/vscode/blob/main/build/win32/code.iss
+"%ASSETSDIR%\vscode-installer.exe" /SILENT /SUPPRESSMSGBOXES /MERGETASKS="!runcode,desktopicon,quicklaunchicon,addcontextmenufiles,addcontextmenufolders,addtopath"
+"%ASSETSDIR%\wireguard-installer.exe" /S
 
-rem PowerShell script block logging
+:: PowerShell script block logging
 powershell.exe -Command "New-Item -Path HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging -Force"
 powershell.exe -Command "Set-ItemProperty -Path HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging -Name EnableScriptBlockLogging -Value 1 -Force"
--- windows/sandbox/get-asset.ps1
+++ windows/sandbox/get-asset.ps1
@@ -4,15 +4,19 @@ function Get-Asset {
   Push-Location -Path 'Assets';
 
   $scriptBlockList += {
-    Invoke-WebRequest -Uri 'https://update.code.visualstudio.com/latest/win32-x64-user/stable' `
+    Invoke-WebRequest -Uri 'https://sourceforge.net/projects/sevenzip/files/latest/download' `
       -Headers @{ 'User-Agent' = 'Wget x64' } `
-      -OutFile 'vscode.exe';
+      -OutFile '7zip-installer.exe';
   }
 
   $scriptBlockList += {
-    Invoke-WebRequest -Uri 'https://sourceforge.net/projects/sevenzip/files/latest/download' `
-      -Headers @{ 'User-Agent' = 'Wget x64' } `
-      -OutFile '7z-x64.exe';
+    Invoke-WebRequest -Uri 'https://update.code.visualstudio.com/latest/win32-x64-user/stable' `
+      -OutFile 'vscode-installer.exe';
+  }
+
+  $scriptBlockList += {
+    Invoke-WebRequest -Uri 'https://download.wireguard.com/windows-client/wireguard-installer.exe' `
+      -OutFile 'wireguard-installer.exe';
   }
 
   $scriptBlockList | ForEach-Object { Start-Job -ScriptBlock $_ };
