63a4dd52710c9fa41fb5aed68d4a504a2b61e6c7
Hoang NGUYEN <10150902+hoang-himself@users.noreply.github.com>
2025-03-07 10:44:59 +0700

    refactor(*)!: drop pyenv
    ```shell
    rm -f $ZDOTDIR/zshrc.d/python-venv.zsh
    ````
    
    ```powershell
    $PROFILE_HOME = Split-Path -Parent $Profile
    Remove-item -Path $PROFILE_HOME\profile.d\python-venv.psm1 -Force
    ````

--- linux/centos.sh
+++ linux/centos.sh
@@ -11,21 +11,9 @@ function install_base {
     neovim logrotate fail2ban
 }
 
-function install_pyenv {
-  sudo dnf install -y make gcc zlib-devel bzip2 bzip2-devel \
-    readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel
-
-  curl -SL https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-  export PYENV_ROOT="$HOME/.pyenv"
-  export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init --path)"
-  eval "$(pyenv init -)"
-}
-
 function main {
   install_base
   install_prompt
-  install_pyenv
 
   set_base
   set_shell
--- linux/fedora-iot.sh
+++ linux/fedora-iot.sh
@@ -9,18 +9,9 @@ function install_base {
     git git-lfs neovim logrotate fail2ban
 }
 
-function install_pyenv {
-  curl -SL https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-  export PYENV_ROOT="$HOME/.pyenv"
-  export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init --path)"
-  eval "$(pyenv init -)"
-}
-
 function main {
   install_base
   install_prompt
-  install_pyenv
 
   set_base
   set_shell
