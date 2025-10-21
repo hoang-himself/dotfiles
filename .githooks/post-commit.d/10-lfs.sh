804d9209162679650aa9919154e352e6d8565e2d
Hoang NGUYEN <10150902+hoang-himself@users.noreply.github.com>
2025-10-21 23:39:58 +0700

    chore(githooks): enable lfs

---
+++ .githooks/post-commit.d/10-lfs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if ! command -v git-lfs >/dev/null 2>&1; then
+  echo -e '\033[31m-----------------------------------------------------\033[0m' >&2
+  echo -e '\033[31mGIT LFS NOT FOUND\033[0m' >&2
+  echo -e "\033[33mThis repository is configured for Git LFS but 'git-lfs' was not found on your path.\033[0m" >&2
+  echo -e "\033[33mIf you no longer wish to use Git LFS, remove this hook by deleting the 'post-commit' file in the hooks directory.\033[0m" >&2
+  echo -e '\033[31m-----------------------------------------------------\033[0m' >&2
+  exit 2
+fi
+
+exec git lfs post-commit "$@"
