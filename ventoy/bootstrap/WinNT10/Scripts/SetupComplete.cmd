f30a5409c985304623131de59113f9459794ba57
Hoang NGUYEN <10150902+hoang-himself@users.noreply.github.com>
2025-11-02 00:06:33 +0700

    feat(ventoy/winnt10): prepare and install office via ODT

--- ventoy/bootstrap/WinNT10/Scripts/SetupComplete.cmd
+++ ventoy/bootstrap/WinNT10/Scripts/SetupComplete.cmd
@@ -1,19 +1,12 @@
 @echo off
 
-:: Precondition
+::
 fltmc >nul || exit /b
 
 :: Set paths
 set SETUPDIR=%SystemRoot%\Setup
 set ASSETSDIR=%SETUPDIR%\Assets
 set SCRIPTSDIR=%SETUPDIR%\Scripts
-
-:: Office
-::start /wait cmd /c "%ASSETSDIR%\Office\start_setup.cmd"
-
-:: MAS
-::curl -SL -o "%SCRIPTSDIR%\MAS_AIO.cmd" https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd
-::start /wait cmd /c "%SCRIPTSDIR%\MAS_AIO.cmd" /HWID /Ohook
 
 :: Visual C++ Redistributable
 ::curl -SL -o "%ASSETSDIR%\VisualCppRedist_AIO_x86_x64.exe" https://github.com/abbodi1406/vcredist/releases/latest/download/VisualCppRedist_AIO_x86_x64.exe
@@ -27,7 +20,16 @@ set SCRIPTSDIR=%SETUPDIR%\Scripts
 ::curl -H "User-Agent: Wget x64" -SL -o "%ASSETSDIR%\NanaZip.msixbundle" https://sourceforge.net/projects/nanazip/files/latest/download
 ::powershell.exe -NoProfile -Command "Add-AppxPackage -Path %ASSETSDIR%\NanaZip.msixbundle;"
 
-:: Postcondition
+:: Office
+::curl -SL -o "%ASSETSDIR%\setup.exe" https://officecdn.microsoft.com/pr/wsus/setup.exe
+::"%ASSETSDIR%\Office\setup.exe" /download "%ASSETSDIR%\Office\O365ProPlusRetail.xml"
+::"%ASSETSDIR%\Office\setup.exe" /configure "%ASSETSDIR%\Office\O365ProPlusRetail.xml"
+
+:: MAS
+::curl -SL -o "%SCRIPTSDIR%\MAS_AIO.cmd" https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd
+::start /wait cmd /c "%SCRIPTSDIR%\MAS_AIO.cmd" /HWID /Ohook
+
+::
 if exist "%SETUPDIR%\CreateSetup.cmd" del /Q "%SETUPDIR%\CreateSetup.cmd"
 if exist "%ASSETSDIR%" rd /S /Q "%ASSETSDIR%"
 for /f "delims=" %%i in ('dir "%SCRIPTSDIR%" /b /a-d ^| findstr /vile ".log" ^| findstr /vile "UserOnce.ps1"') do del "%SCRIPTSDIR%\%%i"
