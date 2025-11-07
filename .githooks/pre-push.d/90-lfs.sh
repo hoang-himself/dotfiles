dca9ebe7353c7520fdabf663260ae02ef32cc9bc
Hoang NGUYEN <10150902+hoang-himself@users.noreply.github.com>
2025-11-07 22:18:24 +0700

    chore(githooks): move pre-push lfs into dedicated file

--- .githooks/pre-push.d/90-lfs.sh
+++ .githooks/pre-push.d/90-lfs.sh
@@ -2,4 +2,15 @@
 
 set -euo pipefail
 
-exit 0
+if ! command -v git-lfs >/dev/null 2>&1; then
+  echo -e '\033[31m-----------------------------------------------------\033[0m' >&2
+  echo -e '\033[31mGIT LFS NOT FOUND\033[0m' >&2
+  echo -e "\033[33mThis repository is configured for Git LFS but 'git-lfs' was not found on your path.\033[0m" >&2
+  echo -e "\033[33mIf you no longer wish to use Git LFS, remove this hook by deleting the 'pre-push' file in the hooks directory.\033[0m" >&2
+  echo -e '\033[31m-----------------------------------------------------\033[0m' >&2
+  exit 2
+fi
+
+tmpfile="$1"
+
+exec git lfs pre-push "$@" < "$tmpfile"
