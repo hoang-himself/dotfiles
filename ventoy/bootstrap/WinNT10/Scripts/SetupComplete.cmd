f43c14bf529b88c806cefc6db26fa889e4953765
Hoang NGUYEN <10150902+hoang-himself@users.noreply.github.com>
2025-11-08 23:07:55 +0700

    feat(ventoy/winnt10): offload get assets to pwsh

--- ventoy/bootstrap/WinNT10/Scripts/SetupComplete.cmd
+++ ventoy/bootstrap/WinNT10/Scripts/SetupComplete.cmd
@@ -14,44 +14,29 @@ fltmc.exe >nul || (echo Not running as administrator. Exiting... >> "%LOGFILE%" 
 
 ::
 echo Installing Visual C++ Redistributable... >> "%LOGFILE%" 2>&1
-goto install_vcredist
-curl.exe -SL --connect-timeout 10 -o "%ASSETSDIR%\VisualCppRedist_AIO_x86_x64.exe" https://github.com/abbodi1406/vcredist/releases/latest/download/VisualCppRedist_AIO_x86_x64.exe
-:install_vcredist
 "%ASSETSDIR%\VisualCppRedist_AIO_x86_x64.exe" /ai9 >> "%LOGFILE%" 2>&1
 
 ::
 echo Installing WinGet... >> "%LOGFILE%" 2>&1
-goto install_winget
-curl.exe -SL --connect-timeout 10 -o "%ASSETSDIR%\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-:install_winget
 powershell.exe -NoProfile -Command "Add-AppxPackage -Path %ASSETSDIR%\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle;" >> "%LOGFILE%" 2>&1
 
 ::
 echo Installing NanaZip... >> "%LOGFILE%" 2>&1
-goto install_nanazip
-curl.exe -SL --connect-timeout 10 -H "User-Agent: Wget x64" -o "%ASSETSDIR%\NanaZip.msixbundle" https://sourceforge.net/projects/nanazip/files/latest/download
-:install_nanazip
-powershell.exe -NoProfile -Command "Add-AppxPackage -Path '%ASSETSDIR%\NanaZip.msixbundle';" >> "%LOGFILE%" 2>&1
+powershell.exe -NoProfile -Command "Add-AppxPackage -Path '%ASSETSDIR%\40174MouriNaruto.NanaZip_gnj4mf6z9tkrc.msixbundle';" >> "%LOGFILE%" 2>&1
 
 ::
 echo Installing Office... >> "%LOGFILE%" 2>&1
-goto install_o365
-curl.exe -SL --connect-timeout 10 -o "%ASSETSDIR%\setup.exe" https://officecdn.microsoft.com/pr/wsus/setup.exe
-"%ASSETSDIR%\Office\setup.exe" /download "%ASSETSDIR%\Office\O365ProPlusRetail.xml"
-:install_o365
 "%ASSETSDIR%\Office\setup.exe" /configure "%ASSETSDIR%\Office\O365ProPlusRetail.xml" >> "%LOGFILE%" 2>&1
 
 ::
 echo Running Microsoft Activation Scripts... >> "%LOGFILE%" 2>&1
-goto install_mas
-curl.exe -SL --connect-timeout 10 -o "%SCRIPTSDIR%\MAS_AIO.cmd" https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd
-:install_mas
 start /wait cmd /c "%SCRIPTSDIR%\MAS_AIO.cmd" /HWID /Ohook >> "%LOGFILE%" 2>&1
 
 ::
 echo SetupComplete.cmd finished successfully. Running cleanup... >> "%LOGFILE%" 2>&1
 
 if exist "%SETUPDIR%\CreateSetup.cmd" del /Q "%SETUPDIR%\CreateSetup.cmd"
+if exist "%SETUPDIR%\get-asset.ps1" del /Q "%SETUPDIR%\get-asset.ps1"
 if exist "%ASSETSDIR%" rd /S /Q "%ASSETSDIR%"
 for /f "delims=" %%i in ('dir "%SCRIPTSDIR%" /b /a-d ^| findstr /vile ".log" ^| findstr /vile "UserOnce.ps1"') do del "%SCRIPTSDIR%\%%i"
 
---
+++ ventoy/bootstrap/WinNT10/get-asset.ps1
@@ -0,0 +1,67 @@
+function Get-Asset {
+  $scriptBlockList = @();
+
+  Push-Location -Path 'Assets';
+
+  New-Item -ItemType Directory -Path 'winget-cli' -Force;
+
+  $scriptBlockList += {
+    Invoke-WebRequest -Uri 'https://github.com/abbodi1406/vcredist/releases/latest/download/VisualCppRedist_AIO_x86_x64.exe' `
+      -OutFile 'VisualCppRedist_AIO_x86_x64.exe';
+  }
+
+  $scriptBlockList += {
+    [array](Invoke-RestMethod -Method 'GET' -Uri 'https://api.github.com/repos/M2Team/NanaZip/releases') `
+    | Where-Object -FilterScript { -not $_.prerelease } `
+    | Select-Object -First 1 -ExpandProperty assets `
+    | Where-Object -FilterScript { $_.name -like '*.msixbundle' } `
+    | Select-Object -ExpandProperty browser_download_url `
+    | ForEach-Object { Invoke-WebRequest -Uri "$_" -OutFile '40174MouriNaruto.NanaZip_gnj4mf6z9tkrc.msixbundle'; }
+  }
+
+  $scriptBlockList += {
+    Invoke-WebRequest -Uri 'https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml' `
+      -OutFile 'microsoft.ui.xaml.nupkg';
+
+    Expand-Archive -Path 'microsoft.ui.xaml.nupkg' -DestinationPath 'microsoft.ui.xaml' -Force;
+    Remove-Item -Path 'microsoft.ui.xaml.nupkg' -Force;
+
+    Copy-Item -Path 'microsoft.ui.xaml\tools\AppX\x64\Release\*' -Recurse -Destination 'winget-cli\' -Force;
+    Remove-Item -Path 'microsoft.ui.xaml' -Recurse -Force;
+  }
+
+  $scriptBlockList += {
+    Invoke-WebRequest -Uri 'https://github.com/microsoft/winget-cli/releases/latest/download/DesktopAppInstaller_Dependencies.zip' `
+      -OutFile 'DesktopAppInstaller_Dependencies.zip';
+
+    Expand-Archive -Path '.\DesktopAppInstaller_Dependencies.zip' -DestinationPath '.\DesktopAppInstaller_Dependencies' -Force;
+    Remove-Item -Path 'DesktopAppInstaller_Dependencies.zip' -Force;
+
+    Copy-Item -Path 'DesktopAppInstaller_Dependencies\x64\*' -Recurse -Destination 'winget-cli\' -Force;
+    Remove-Item -Path 'DesktopAppInstaller_Dependencies' -Recurse -Force;
+  }
+
+  $scriptBlockList += {
+    Invoke-WebRequest -Uri 'https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' `
+      -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle';
+  }
+
+  $scriptBlockList += {
+    Invoke-WebRequest -Uri 'https://officecdn.microsoft.com/pr/wsus/setup.exe' `
+      -OutFile 'Office\setup.exe';
+    Start-Process -WorkingDirectory '.\Office' -FilePath 'setup.exe' -ArgumentList @('/download', 'O365ProPlusRetail.xml') -WindowStyle Minimized;
+  }
+
+  $scriptBlockList += {
+    Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd' `
+      -OutFile 'MAS_AIO.cmd';
+  }
+
+  $scriptBlockList | ForEach-Object { Start-Job -ScriptBlock $_ };
+
+  Get-Job | Wait-Job;
+
+  Pop-Location;
+}
+
+Get-Asset;
