13ece578c1cd7faeb8db9b88215e0cc9c1e5b12d
Hoang NGUYEN <10150902+hoang-himself@users.noreply.github.com>
2026-03-06 16:26:50 +0700

    docs(common/git): set no push by default

---
+++ .githooks/pre-push.d/00-assert-push-is-allowed.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# remote_name="$1"
+# remote_url="$2"
+
+# patterns=(
+#   'blocked|github.com/work-org/'
+#   'blocked|gitlab.example.com'
+#   'allowed|git@github.com:hoang-himself/'
+#   'allowed|https://github.com/hoang-himself/'
+# )
+
+# policy=''
+
+# for pattern_entry in "${patterns[@]}"; do
+#   type="${pattern_entry%|*}"
+#   pattern="${pattern_entry#*|}"
+
+#   if [[ "$remote_url" == *"$pattern"* ]]; then
+#     policy="$type"
+#     break
+#   fi
+# done
+
+# if [[ -z "$policy" ]] || [[ "$policy" == 'blocked' ]]; then
+#   echo -e '\033[31m-----------------------------------------------------\033[0m' >&2
+#   echo -e '\033[31mPUSH REJECTED\033[0m' >&2
+#   echo -e "\033[33mPushing to $remote_name <$remote_url> is not allowed.\033[0m" >&2
+#   echo -e '\033[31m-----------------------------------------------------\033[0m' >&2
+#   exit 1
+# fi
